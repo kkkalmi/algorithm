cc135a835ddef8400f977ee790979a50abf967fe
homekgu <homekgu@kyonggi.ac.kr>
2024-11-15 22:51:23 +0900

    Solve swea_1976

---
+++ swea_1976.py
@@ -0,0 +1,14 @@
+for test_case in range(1, T + 1):
+    h, m, h1, m2 = map(int, input().split())
+    
+    result_hour = h + h1
+    
+    result_minute = m + m2
+    
+    if result_hour > 12:
+        result_hour = result_hour % 12
+    if result_minute > 60:
+        result_hour += 1
+        result_minute = result_minute % 60
+        
+    print(f"#{test_case} {result_hour} {result_minute}")
