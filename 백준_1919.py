925a7526f7988231156726b488b93a247bc09687
homekgu <homekgu@kyonggi.ac.kr>
2024-11-18 10:38:50 +0900

    Refactor 백준 1919

--- 백준_1919.py
+++ 백준_1919.py
@@ -3,4 +3,4 @@ from collections import Counter
 N_dict = Counter(input())
 M_dict = Counter(input())
 
-print(sum((N_dict-M_dict).values()) + sum((M-dict-N_dict).values()))
+print(sum((N_dict-M_dict).values()) + sum((M_dict-N_dict).values()))
