8f7ff9a96cb46f15ab7363d464470930d3f29b47
homekgu <homekgu@kyonggi.ac.kr>
2024-12-04 22:20:30 +0900

    햄버거 문제 수정

--- ham.py
+++ ham.py
@@ -1,27 +1,26 @@
- import sys
-sys.stdin = open("input.txt", 'r')
+import sys.stdin 
 
 T = int(input())
 
 for tc in range(1, T + 1):
-N, L = map(int, input().split())
-ingredients = [list(map(int, input().split())) for _ in range(N)] # T:맛 점수, K:재료 칼로리
+    N, L = map(int, input().split())
+    ingredients = [list(map(int, input().split())) for _ in range(N)] # T:맛 점수, K:재료 칼로리
 
 max_taste_score = 0
 
 for bitmask in range(1 << N):
-total_taste = 0
-total_calories = 0
+    total_taste = 0
+    total_calories = 0
 
 for i in range(N):
-if bitmask & (1 << i):
-total_taste += ingredients[i][0]
-total_calories += ingredients[i][1]
+    if bitmask & (1 << i):
+        total_taste += ingredients[i][0]
+        total_calories += ingredients[i][1]
 
-if total_calories > L:
-continue
+    if total_calories > L:
+        continue
 
-if total_calories <= L:
-max_taste_score = max(max_taste_score, total_taste)
+    if total_calories <= L:
+        max_taste_score = max(max_taste_score, total_taste)
 
 print("#{} {}".format(tc, max_taste_score))
