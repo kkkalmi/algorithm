42b9ec0c744986f429b32aab2b0e7020aae565e8
homekgu <homekgu@kyonggi.ac.kr>
2024-12-06 23:11:09 +0900

    Solve 백준 10870

---
+++ 백준_10870.py
@@ -0,0 +1,14 @@
+def fibo(n):
+    if n == 1:
+        return 1
+    elif n == 0:
+        return 0
+    else:
+        return fibo(n-2) + fibo(n-1)
+
+
+user = int(input())
+
+result = fibo(user)
+
+print(result)
